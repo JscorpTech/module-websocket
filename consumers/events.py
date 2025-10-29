c6c6b9788fe5b79d57a9a575d1d593b50f493c5d
A'zamov Samandar <admin@jscorp.uz>
2025-10-29 18:45:57 +0500

    fix typo

--- consumers/events.py
+++ consumers/events.py
@@ -7,17 +7,21 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class EventsConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         user = self.scope.get("user")
+        self.user = user
         if user is None or not user.is_authenticated:
             logging.warning("Muvofaqiyatsiz ulanishga urunish")
             await self.close(4001)
             return
         logging.info("Yangi ulanish user=%s", user.pk)
-        self.user = user
         await self.channel_layer.group_add("chat", self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        logging.info("Websocket uzulish code=%s user=%s", close_code, self.user.pk)
+        logging.info(
+            "Websocket uzulish code=%s user=%s",
+            close_code,
+            self.user.pk if self.user is not None else None,
+        )
         await self.channel_layer.group_discard("chat", self.channel_name)
 
     async def receive(self, text_data):
